609ebfd205d6cb5fcfbc0c005e66908a024308c9
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2022-03-15 21:19:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,19 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Inches
     )
-    if (distance <= 30 && distance >= 20) {
-        music.playTone(349, music.beat(BeatFraction.Whole))
+    if (distance <= 30 && distance > 20) {
+        music.setVolume(130)
+        music.setTempo(100)
+        music.playTone(698, music.beat(BeatFraction.Half))
+    } else if (distance <= 20 && distance > 10) {
+        music.setVolume(140)
+        music.setTempo(116)
+        music.playTone(698, music.beat(BeatFraction.Half))
+    } else if (distance <= 10) {
+        music.setVolume(150)
+        music.setTempo(125)
+        music.playTone(698, music.beat(BeatFraction.Half))
+    } else {
+        music.stopAllSounds()
     }
 })
